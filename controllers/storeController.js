283d2e15efee694615f49070af709965c1bb0077
Mohit Garg <gargmohit@live.in>
2017-05-09 01:13:40 +0530

    Add new store route and template added

---
+++ controllers/storeController.js
@@ -0,0 +1,13 @@
+exports.homepage = (req, res) => {
+    res.render('index')
+}
+
+exports.addStore = (req, res) => {
+    res.render('editStore', {
+        title:'Add Store'
+    })
+}
+
+exports.createStore = (req, res) => {
+    res.json(req.body)
+}
