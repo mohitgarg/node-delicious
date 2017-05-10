484059051891890ebc65f241b2a41184d35f0a4f
Mohit Garg <gargmohit@live.in>
2017-05-10 23:19:36 +0530

    forgot password form added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport')
+const crypto = require('crypto')
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
 
 exports.login = passport.authenticate('local',{
     failureRedirect:'/login',
@@ -11,4 +14,41 @@ exports.logout = (req, res) => {
     req.logout();
     req.flash('success', 'You are now logged out!👏')
     res.redirect('/')
+}
+
+exports.isLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated()){
+        next();
+        return
+    }
+    req.flash('error', 'You must be logged in to do that!');
+    res.redirect('/login')
+}
+
+exports.forgot = async (req, res) => {
+    const user = await User.findOne({email:req.body.email})
+    if(!user){
+        req.flash('error', 'A password reset link has been sent to you')
+        return res.redirect('/login')
+    }
+    user.resetPasswordToken = crypto.randomBytes(20).toString('hex')
+    user.resetPasswordExpires = Date.now() + 3600000   // Now + 1 hour
+    await user.save();
+
+    const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`
+
+    req.flash('success', `A password reset link has been emailed to you. ${resetURL}`);
+
+    res.redirect('/login')
+}
+
+exports.reset = async (req, res) => {
+    const user = await User.findOne({
+        resetPasswordToken:req.params.token,
+        resetPasswordExpires: {$gt: Date.now() }
+    })
+
+    if(!user) {
+        
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const { catchErrors } = require('../handlers/errorHandlers')
 // Do work here
 router.get('/',catchErrors(storeController.getStores))
 router.get('/stores',catchErrors(storeController.getStores))
-router.get('/add',storeController.addStore)
+router.get('/add',authController.isLoggedIn,storeController.addStore)
 router.post('/add',storeController.upload, catchErrors(storeController.resize) ,catchErrors(storeController.createStore));
 router.post('/add/:id', storeController.upload, catchErrors(storeController.resize), catchErrors(storeController.updateStore));
 router.get('/stores/:id/edit', catchErrors(storeController.editStore))
@@ -16,7 +16,12 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug))
 router.get('/tags', catchErrors(storeController.getStoresByTag))
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag))
 router.get('/login', userController.loginForm)
+router.post('/login', authController.login)
 router.get('/register', userController.registerForm)
 router.post('/register', userController.validateRegister, userController.register, authController.login)
 router.get('/logout', authController.logout)
+router.get('/account',authController.isLoggedIn,userController.account)
+router.post('/account',catchErrors(userController.updateAccount))
+router.post('/account/forgot', catchErrors(authController.forgot))
+router.get('/account/reset/:token', catchErrors(authController.reset))
 module.exports = router;
